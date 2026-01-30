f58cc09cec081caed7d163118e3a11806902a93c
Rohitha0105 <160598339+Rohitha0105@users.noreply.github.com>
2026-01-30 11:44:27 +0530

    Dictionary

---
+++ dict.py
@@ -0,0 +1,13 @@
+details={
+"name": "surya",
+"employed": False,
+"batch":7,
+"address": {
+"dr_no":"3-91",
+"raod":1,
+"area":"sainath nagar",
+"city":"hyd",
+"pincode":500074
+}
+}
+print(details ["address"]["pincode"])
